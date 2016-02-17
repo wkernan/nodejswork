c2326fdee574acc1c250024adf6ab91b0f31a44a
wkernan@gmail.com <wkernan@gmail.com>
2016-02-17 15:55:39 -0600

    first init

---
+++ README.md
@@ -0,0 +1,3 @@
+# nodejswork
+# nodejswork
+# nodejswork
---
+++ routes/users.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+
+/* Get userlist */
+router.get('/userlist', function(req, res) {
+	var db = req.db;
+	var collection = db.get('userlist');
+	collection.find({},{},function(e,docs){
+		res.json(docs);
+	});
+});
+
+/* Post to adduser */
+router.post('/adduser', function(req, res) {
+	var db = req.db;
+	var collection = db.get('userlist');
+	collection.insert(req.body, function(err, result) {
+		res.send(
+			(err === null) ? { msg: '' } : { msg: err }
+		);
+	});
+});
+
+/* PUT to editUser */
+router.put('/edituser/:id', function(req, res) {
+	var db = req.db;
+	var collection = db.get('userlist');
+	var userToEdit = req.params.id;
+	collection.update({ '_id' : userToEdit }, { $set: req.body }, function(err, result) {
+		res.send(
+			(err === null) ? { msg: ''} : { msg: err }	
+		);
+	});
+});
+
+/* DELETE to deleteuser */
+router.delete('/deleteuser/:id', function(req, res) {
+	var db = req.db;
+	var collection = db.get('userlist');
+	var userToDelete = req.params.id;
+	collection.remove({ '_id' : userToDelete }, function(err) {
+		res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+	});
+});
+
+module.exports = router;
